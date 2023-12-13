433b9aad60691d6eb48bfe6cc783eebcb3cde11a
Scotttmcevoy <scotttmcevoy@gmail.com>
2023-12-12 21:21:56 -0500

    Initial commit

---
+++ pages/urls.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from django.urls import path, include
+
+from django.urls import path
+from .views import HomePageView, AboutPageView
+
+urlpatterns = [
+    path("about/", AboutPageView.as_view(), name="about"),
+    path("", HomePageView.as_view(), name="home"),
+
+]
